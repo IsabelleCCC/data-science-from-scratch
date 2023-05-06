e6085b9aa5a90fb0625ad3cbf538447f61affe2c
IsabelleCCC <isa.ccc@live.com>
2023-05-06 01:15:28 -0300

    Álgebra linear - matrizes

--- algebra-linear.py
+++ algebra-linear.py
@@ -35,3 +35,70 @@ def scalar_multiply(c: float, v: Vector) -> Vector:
     return [i * c for i in v]
 
 assert scalar_multiply(2, [1, 2, 3]) == [2, 4, 6]
+
+def vector_mean(vectors: List[Vector]) -> Vector:
+    ''' Computa a média dos elementos '''
+    n = len(vectors)
+    return scalar_multiply(1/n, vector_sum(vectors))
+
+assert vector_mean([[1, 2], [3, 4], [5, 6]]) == [3, 4]
+
+def dot(v: Vector, w: Vector) -> float:
+    """Computes v_1 * w_1 + ... + v_n * w_n"""
+    assert len(v) == len(w), "vectors must be same length"
+
+    return sum(v_i * w_i for v_i, w_i in zip(v, w))
+
+assert dot([1, 2, 3], [4, 5, 6]) == 32
+
+def sum_of_squares(v: Vector) -> float:
+    ''' Returns v_1 * v_1 + ... v_n * v_n '''
+    return dot(v, v)
+
+assert sum_of_squares([1, 2, 3]) == 14
+
+import math
+
+def magnitude(v: Vector) -> float:
+	return math.sqrt(sum_of_squares(v))
+
+assert magnitude([3, 4]) == 5
+
+def squared_distance(v: Vector, w: Vector) -> float:
+     ''' Computes (v_1 - w_1) ** 2 + ... + (v_n - w_n) ** 2 '''
+     return sum_of_squares(subtract(v, w))
+
+def distance(v: Vector, w: Vector) -> float:
+     ''' Computes distance between v and w '''
+     return math.sqrt(squared_distance(v, w))
+
+# or
+
+def distance(v: Vector, w: Vector):
+     return magnitude(subtract(v , w))
+
+Matrix = List[List[float]]
+
+A = [[1, 2, 3],
+     [4, 5, 6]]
+
+B = [[1, 2],
+     [3, 4],
+      [5, 6]]
+
+from typing import Tuple
+
+def shape(A: Matrix) -> Tuple[int, int]:
+     '''' Returns n of rows and n of columns '''
+     num_rows = len(A)
+     num_cols = len(A[0]) if A else 0
+     return num_rows, num_cols
+
+assert shape([[1, 2, 3], [4, 5, 6]]) == (2 , 3)
+
+def get_row(A: Matrix, i: int) -> Vector:
+     return A[i]
+
+def get_col(A: Matrix, j: int) -> Vector:
+     return [A_i[j] for A_i in A]
+
