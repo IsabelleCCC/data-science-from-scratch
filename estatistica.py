18c43c70b14a48d6b623f5023b2a804bfadac8fe
IsabelleCCC <isa.ccc@live.com>
2023-05-13 01:00:35 -0300

    Estatística - quantil, moda e amplitude

--- estatistica.py
+++ estatistica.py
@@ -1,7 +1,7 @@
 from collections import Counter
 import matplotlib.pyplot as plt
 
-num_friends = [100.0,49,41,40,25,21,21,19,19,18,18,16,15,15,15,15,14,14,13,13,13,13,12,12,11,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,8,8,8,8,8,8,8,8,8,8,8,8,8,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+num_friends = [100.0,49,41,40,25,21,21,19,19,18,18,16,15,15,15,15,14,14,13,13,13,13,12,12,11,10 	,10,10,10,10,10,10,10,10,10,10,10,10,10,10,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,8,8,8,8,8,8,8,8,8,8,8,8,8,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 
 friend_counts = Counter(num_friends)
 xs = range(101)
@@ -48,3 +48,28 @@ def median(v: List[float]) -> float:
 
 assert median([1, 10, 2, 9, 5]) == 5
 assert median([1, 9, 2, 10]) == (2 + 9) / 2
+
+print(median(num_friends))
+
+def quantile(xs: List[float], p: float) -> float:
+    p_index = int(p * len(xs))
+    return sorted(xs)[p_index]
+
+assert quantile(num_friends, 0.10) == 1
+assert quantile(num_friends, 0.25) == 3
+assert quantile(num_friends, 0.75) == 9
+assert quantile(num_friends, 0.90) == 13
+
+def mode(x: List[float]) -> List[float]:
+      ''' Cálculo de moda. Retorna o número amigos mais frequente. '''
+      count_friends = Counter(x)
+      max_count = max(count_friends.values())
+      return [x_i for x_i, count in count_friends.items() if count == max_count]
+
+assert mode(num_friends) == [6,1]  
+
+def data_range(xs: List[float]) -> float:
+      ''' Cálculo da amplitude '''
+      return max(xs) - min(xs)
+
+assert data_range(num_friends) == 99
